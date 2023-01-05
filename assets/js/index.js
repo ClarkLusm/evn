551c6a17d2b9d975385b5a28dd7a26a14fbecc56
Thanh Phan <phanthanh.humg@gmail.com>
2023-01-05 11:05:37 +0700

    Change select to multiple select

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,43 @@ function goZone(index) {
     window.location.href = "zone-hanoi.html";
   }
 }
+$('select.selectpicker').selectpicker({
+  maxOptions: 3
+});
+$('.selectpicker').on('change', function (e, clickedIndex, isSelected, previousValue) {
+  const values = $(this).val();
+  if (values.length > 1) {
+    if ($(this).hasClass('province')) {
+      $('.district').attr('disabled', true);
+      $('.ward').attr('disabled', true);
+    }
+    if ($(this).hasClass('district')) {
+      $('.ward').attr('disabled', true);
+    }
+  } else {
+    if ($(this).hasClass('province')) {
+      $('.district').removeAttr('disabled');
+      $('.ward').removeAttr('disabled');
+    }
+    if ($(this).hasClass('district')) {
+      $('.ward').removeAttr('disabled');
+    }
+  }
+  toggleOption(this, values);
+  $('.selectpicker').selectpicker('refresh');
+});
+
+
+function toggleOption(self, selectedValues) {
+  if (selectedValues.length >= 3) {
+    $(self).find('option').each(function (_, item) {
+      if (!selectedValues.includes($(item).val())) {
+        $(item).attr('disabled', true);
+      }
+    });
+  } else {
+    $(self).find('option').each(function (_, item) {
+      $(item).removeAttr('disabled');
+    });
+  }
+}
